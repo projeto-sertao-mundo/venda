603b2cac9876bf73a373bd70864d7a69beb4a722
Jonathan Bergson <developer@bergson.me>
2021-03-30 10:41:32 -0300

    update: novas consfigurações

--- game/scripts/button-actions.gd
+++ game/scripts/button-actions.gd
@@ -5,24 +5,18 @@ func _on_PlayGame_pressed():
 
 func _on_BackMenu_pressed():
 	get_tree().change_scene("res://scenes/menu.tscn")
-	pass
 
 func _on_BackStoreOut_pressed():
 	get_tree().change_scene("res://scenes/store-out.tscn")
-	pass
 
 func _on_BackLevelChoice_pressed():
 	get_tree().change_scene("res://scenes/levels.tscn")
-	pass
 
 func _on_EnterStore_pressed():
 	get_tree().change_scene("res://scenes/store.tscn")
-	pass
 
 func _on_EnterLevelChoice_pressed():
 	get_tree().change_scene("res://scenes/levels.tscn")
-	pass
 
 func _on_EnterLevel01_pressed():
 	get_tree().change_scene("res://scenes/level-01.tscn")
-	pass
