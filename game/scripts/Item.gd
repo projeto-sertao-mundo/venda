b167da8d2a1b1603b1d45831bd8cf8f2b0d2db22
Jonathan Bergson <contato@bergson.me>
2021-05-18 20:26:02 -0300

    feat: animando itens da gruta

--- game/scripts/Item.gd
+++ game/scripts/Item.gd
@@ -1,8 +1,13 @@
-extends Area2D
+extends KinematicBody2D
 
 onready var manager = get_node("/root/Manager")
 export (String, "Cave", "Church", "Elephant", "Station") var level = ""
 export (String) var item_name = ""
+
+var is_founded = false
+var target = Vector2(1220, 670)
+var velocity = Vector2()
+var speed = 500
 
 # TODO: Atualizar o nome desse method para _on_click_item
 func _on_item_is_pressed(vi1ewport, event, shape_idx):
@@ -12,7 +17,17 @@ func _on_item_is_pressed(vi1ewport, event, shape_idx):
 
 func founded():
 	manager.item_found(level, item_name)
-	delete_me()
+	z_index = 5000
+	z_as_relative = false
+	is_founded = true
 
 func delete_me():
 	get_node(get_path()).queue_free()
+
+func _physics_process(delta):
+	velocity = position.direction_to(target) * speed
+
+	if is_founded && position.distance_to(target) > 5:
+		velocity = move_and_slide(velocity)
+	elif is_founded:
+		delete_me()
