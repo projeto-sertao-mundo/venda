edfb2c095e6ef00f4717e9c3da15879b3bc9cc62
Jonathan Bergson <contato@bergson.me>
2021-06-08 14:18:49 -0300

    feat: fix muita coisa

--- game/scenes/map/ExitLevel.gd
+++ game/scenes/map/ExitLevel.gd
@@ -2,13 +2,17 @@ extends Node
 
 export (Texture) var sprite_default
 export (Texture) var sprite_hover
+export var exit_to_map: bool = true
 
 onready var sprite_node = get_node("Sprite")
 
 func _on_click_button(event: InputEvent):
 	if event is InputEventMouseButton:
 		if event.is_pressed() && event.get_button_index() == BUTTON_LEFT:
-			get_tree().change_scene("res://scenes/map/Map.tscn")
+			if exit_to_map:
+				get_tree().change_scene("res://scenes/map/Map.tscn")
+			else:
+				get_tree().change_scene("res://scenes/menu/Menu.tscn")
 
 func _on_mouse_entered():
 	sprite_node.texture = sprite_hover
