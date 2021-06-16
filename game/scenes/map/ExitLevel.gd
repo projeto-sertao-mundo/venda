16eaf8b059ae592bcb8fcd83ba1b63b22dd63c7c
Jonathan Bergson <contato@bergson.me>
2021-06-16 14:57:41 -0300

    feat: nova build

--- game/scenes/map/ExitLevel.gd
+++ game/scenes/map/ExitLevel.gd
@@ -12,7 +12,7 @@ func _on_click_button(event: InputEvent):
 			if exit_to_map:
 				get_tree().change_scene("res://scenes/map/Map.tscn")
 			else:
-				get_tree().change_scene("res://scenes/store-out.tscn")
+				get_tree().change_scene("res://scenes/menu/Menu.tscn")
 
 func _on_mouse_entered():
 	sprite_node.texture = sprite_hover
