22e2942188d26a73c40b9ace0b36fbca545b2115
Jonathan Bergson <contato@bergson.me>
2021-07-23 15:36:02 -0300

    feat: exibindo nova mensagem no final do game

--- game/scenes/commerce/Dialog.gd
+++ game/scenes/commerce/Dialog.gd
@@ -1,8 +1,13 @@
 extends Node2D
 
+var complete_dialog_step = 1
+
 onready var item_manager = get_node("/root/Manager")
 onready var complete_dialog = get_node("Complete")
 onready var incomplete_dialog = get_node("Incomplete")
+
+onready var complete_dialog_msg_01 = get_node("Complete/RichTextLabel")
+onready var complete_dialog_msg_02 = get_node("Complete/RichTextLabel2")
 
 func _ready():
 	hidden_all()
@@ -14,7 +19,16 @@ func show_dialog():
 	elif status == false: show_incomplete_dialog()
 
 func show_complete_dialog():
+	hidden_complete_dialog()
 	complete_dialog.visible = true
+	if complete_dialog_step == 1:
+		complete_dialog_step += 1
+		complete_dialog_msg_01.visible = true
+	elif complete_dialog_step == 2:
+		complete_dialog_step += 1
+		complete_dialog_msg_02.visible = true
+	elif complete_dialog_step == 3:
+		hidden_complete_dialog()
 
 func show_incomplete_dialog():
 	incomplete_dialog.visible = true
@@ -26,6 +40,8 @@ func hidden_all():
 
 func hidden_complete_dialog():
 	complete_dialog.visible = false
+	complete_dialog_msg_01.visible = false
+	complete_dialog_msg_02.visible = false
 
 func hidden_incomplete_dialog():
 	incomplete_dialog.visible = false
@@ -33,7 +49,7 @@ func hidden_incomplete_dialog():
 func _on_hidden_complete_dialog(event: InputEvent):
 	if event is InputEventMouseButton:
 		if event.is_pressed() && event.get_button_index() == BUTTON_LEFT:
-			hidden_complete_dialog()
+			show_complete_dialog()
 
 func _on_hidden_incomplete_dialog(event: InputEvent):
 	if event is InputEventMouseButton:
