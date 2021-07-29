aee15685a2dd7f006c262be411bf36f750f2f162
Jonathan Bergson <contato@bergson.me>
2021-07-29 15:43:56 -0300

    feat: animando todos os texto do game

--- game/scenes/commerce/Dialog.gd
+++ game/scenes/commerce/Dialog.gd
@@ -9,9 +9,31 @@ onready var incomplete_dialog = get_node("Incomplete")
 onready var complete_dialog_msg_01 = get_node("Complete/RichTextLabel")
 onready var complete_dialog_msg_02 = get_node("Complete/RichTextLabel2")
 
+var animation_time_ref = 0
+var animation_time_max = 0
+var animation_words_per_secound = 20
+var animation_is_running = false
+onready var text_label = null
+
 func _ready():
+	animation_time_max = 1.0 / animation_words_per_secound
 	hidden_all()
 	show_dialog()
+	
+func _process(delta):
+	if animation_is_running:
+		animation_time_ref += delta
+		if animation_time_ref >= animation_time_max:
+			animation_time_ref = 0
+			text_label.visible_characters += 1
+			if text_label.percent_visible == 1:
+				animation_is_running = false
+
+func start_text_label_animation(label):
+	animation_time_ref = 0
+	animation_is_running = true
+	text_label = label
+	text_label.visible_characters = 0
 
 func show_dialog():
 	var status = item_manager.check_game()
@@ -24,14 +46,18 @@ func show_complete_dialog():
 	if complete_dialog_step == 1:
 		complete_dialog_step += 1
 		complete_dialog_msg_01.visible = true
+		start_text_label_animation(complete_dialog_msg_01)
 	elif complete_dialog_step == 2:
 		complete_dialog_step += 1
 		complete_dialog_msg_02.visible = true
+		start_text_label_animation(complete_dialog_msg_02)
 	elif complete_dialog_step == 3:
 		hidden_complete_dialog()
 
 func show_incomplete_dialog():
 	incomplete_dialog.visible = true
+	var label = incomplete_dialog.get_node("RichTextLabel")
+	start_text_label_animation(label)
 
 func hidden_all():
 	visible = true
